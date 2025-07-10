d0287eea9891804abc7ab12203f6aea25a17c0a2
kaline2004 <k.andriele@escolar.ifrn.edu.br>
2025-07-10 10:54:13 -0300

    Centraliza totalmente as imagens dos banners do carrossel (object-center)

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -255,7 +255,7 @@ const HomePage = () => {
   <img
     src={promo.imagem}
     alt={`Banner ${promo.id}`}
-    className="h-full w-full object-contain bg-black" style={{ objectPosition: '30% center' }}
+    className="h-full w-full object-contain object-center bg-black"
     draggable="false"
     onError={(e) => {
       e.target.style.display = 'none';
